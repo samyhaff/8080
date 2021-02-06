3eeaef17043ded00ef356198c0f82ed293bc77c2
samyhaff <haffoudhisamy@gmail.com>
2021-02-06 22:58:08 +0100

    can now print hex code

--- disassembler8080.c
+++ disassembler8080.c
@@ -12,13 +12,16 @@ int main (int argc, char**argv)
 
     //Get the file size and read it into a memory buffer    
     fseek(f, 0L, SEEK_END);    
-    printf("%d", fsize);
+    int fsize = ftell(f);    
     fseek(f, 0L, SEEK_SET);    
 
     unsigned char *buffer=malloc(fsize);    
 
     fread(buffer, fsize, 1, f);    
     fclose(f);    
+
+    for (int i = 0; i < fsize; i++)
+        printf("%d\n", buffer[i]);
 
     return 0;    
 }    
