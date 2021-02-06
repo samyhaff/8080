79388a0a3d1f507957c8679265cbd035e79f6fff
samyhaff <haffoudhisamy@gmail.com>
2021-02-06 22:50:38 +0100

    read rom

--- disassembler8080.c
+++ disassembler8080.c
@@ -1,13 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main() {
-    FILE *fp;    
-    if ((fp = fopen("test", "r")) == NULL) {
-        printf("failed to read rom\n");
-        exit(0);
-    }
-    printf("%c", getc(fp));
-    printf("%c", getc(fp));
-return 0;
-}
+int main (int argc, char**argv)    
+{    
+    FILE *f= fopen(argv[1], "rb");    
+    if (f==NULL)    
+    {    
+        printf("error: Couldn't open %s\n", argv[1]);    
+        exit(1);    
+    }    
+
+    //Get the file size and read it into a memory buffer    
+    fseek(f, 0L, SEEK_END);    
+    printf("%d", fsize);
+    fseek(f, 0L, SEEK_SET);    
+
+    unsigned char *buffer=malloc(fsize);    
+
+    fread(buffer, fsize, 1, f);    
+    fclose(f);    
+
+    return 0;    
+}    
